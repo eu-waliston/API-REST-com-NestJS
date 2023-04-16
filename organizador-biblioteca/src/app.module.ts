7e37f2e8bbed034185f8b4d8d28cb442cad0731e
walwizard <goneisgone@hotmail.com>
2023-04-16 18:46:11 -0300

    API - REST, DbConnect e Schema

--- organizador-biblioteca/src/app.module.ts
+++ organizador-biblioteca/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './Controllers/books/books.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://wesantosdev:4002892233513341@rest.gadbbad.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [BooksController],
   providers: [],
 })
