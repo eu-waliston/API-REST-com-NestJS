11f8cb6e8b1747ff69a49d073d38dc8f343247b9
walwizard <goneisgone@hotmail.com>
2023-04-16 17:57:41 -0300

    API - REST, CRUD - initial commits

--- organizador-biblioteca/src/Controllers/books/books.controller.ts
+++ organizador-biblioteca/src/Controllers/books/books.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('books')
 export class BooksController {
   @Get()
   getAllBooks(): string {
     return 'Todos os livros estão aqui';
+  }
+
+  @Post()
+  saveBook(): string {
+    return 'Esse livro foi salvo';
+  }
+
+  @Patch()
+  updateBook(): string {
+    return 'Esse livro foi atualiuzado';
+  }
+
+  @Delete()
+  deleteBook(): string {
+    return 'Esse livro foi apagado';
   }
 }
